8804de93926dc679083113e0461b1940c59516c8
Nimira43 <n.rabone72@aol.com>
2025-01-08 03:16:40 +0000

    Setup analysers folder and created WinsAnalysis.ts to check for match wins. Starting to weite new WinAnalysis class.

---
+++ src/analysers/WinsAnalysis.ts
@@ -0,0 +1,10 @@
+import { Analyser } from '../summary'
+import { MatchData } from '../MatchData'
+
+export class WinsAnalysis implements Analyser {
+  constructor(public team: string) {
+  run(matches: MatchData[]) : string {
+
+  }  
+  }
+}
