1defdf38d85b9ffe5f77fba5752046c9434c4784
Nimira43 <n.rabone72@aol.com>
2025-01-08 03:22:38 +0000

    Took out the code from index.ts that checked for Man Utd wins and placed it inside WinsAnalysis.ts and updated it.

--- src/analysers/WinsAnalysis.ts
+++ src/analysers/WinsAnalysis.ts
@@ -1,10 +1,18 @@
 import { Analyser } from '../summary'
 import { MatchData } from '../MatchData'
+import { MatchResult } from '../MatchResult'
 
 export class WinsAnalysis implements Analyser {
-  constructor(public team: string) {
+  constructor(public team: string) {}
   run(matches: MatchData[]) : string {
+    let wins = 0
 
+    for (let match of matches) {
+      if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        wins++
+      } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        wins++
+      }
+    }
   }  
-  }
 }
