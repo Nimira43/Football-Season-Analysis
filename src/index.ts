738ce1b563920c6486e51cc8936b8f7dfa151f48
Nimira43 <n.rabone72@aol.com>
2025-01-08 03:43:48 +0000

    Cleaned up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { MatchReader } from './MatchReader'
 import { CsvFileReader } from './CsvFileReader'
-// import { MatchResult } from './MatchResult'
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
-
-// console.log(`Man United has won ${manUnitedWins} games`)
